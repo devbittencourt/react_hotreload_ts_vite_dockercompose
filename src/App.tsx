2794faecdd8d7ea22ba73186b1b488036952ba54
devBittencourt <124898684+devbittencourt@users.noreply.github.com>
2025-06-23 11:22:16 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      Hello world, this is app created with Vite + React + TypeScript + Docker + docker compose + hot reload ao vivasso novamente
+      novo teste ok
+    </>
+  )
+}
+
+export default App
